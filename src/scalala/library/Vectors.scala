feca61dab8c7e04c2701cc53081fa44c4756bc37
Daniel Ramage <dramage@cs.stanford.edu>
2009-04-09 01:31:11 -0700

    better support of different double groupings

--- src/scalala/library/Vectors.scala
+++ src/scalala/library/Vectors.scala
@@ -20,7 +20,10 @@
 package scalala.library
 
 import scalala.collection.PartialMap;
-import scalala.tensor.Vector;
+
+import scalala.tensor.{Tensor,Vector};
+
+import scalala.tensor.operators.TensorOp;
 
 /**
  * Basic vector functions.
@@ -29,7 +32,8 @@ import scalala.tensor.Vector;
  */
 trait Vectors extends Library with Operators {
   /** 100 evenly spaced points between a and b */
-  def linspace(a : Double, b : Double) : Vector = linspace(a,b,100);
+  def linspace(a : Double, b : Double) : Vector =
+    linspace(a,b,100);
   
   /** n evenly spaced points between a and b */
   def linspace(a : Double, b : Double, n : Int) : Vector = {
@@ -45,48 +49,103 @@ trait Vectors extends Library with Operators {
   /** A vector of zeros of the given size */
   def zeros(n : Int) : Vector = DenseVector(n);
   
-  /** Sums the elements of a vector */
-  def sum[I](v : PartialMap[I,Double]) : Double = {
+  /** Returns the sum of the elements and how many there were. */
+  private def sumcount(v : Iterator[Double]) : (Double,Int) = {
     var sum = 0.0;
-    if (v.default == 0.0) {
-      for (x <- v.activeValues) { sum += x; } 
-    } else if (v.domain.isFinite) {
-      var numActive = 0;
-      for (x <- v.activeValues) {
-        sum += x;
-        numActive += 1;
-      } 
-      sum += (v.domain.asInstanceOf[scalala.collection.domain.FiniteDomain[_]].size - numActive) * v.default;
-    } else {
-      for (x <- v.values) { sum += x; }
+    var count = 0;
+    for (x <- v) {
+      sum += x;
+      count += 1;
     }
-    return sum;
+    return (sum,count);
   }
   
-  /** Log each element of a vector or matrix */
-  def log(v : Vector) : Vector = {
-    val x = v.copy.asInstanceOf[Vector];
-    x := v.map(Math.log _);
-    x;
+  /**
+   * Returns the sum of the elements in the map and how many there were.  If the
+   * map has an infinite domain, return -1 as count.
+   */
+  private def sumcount[I](v : PartialMap[I,Double]) : (Double,Int) = {
+    val domainsize =
+      if (v.domain.isFinite)
+        v.domain.asInstanceOf[scalala.collection.domain.FiniteDomain[_]].size
+      else -1;
+      
+    if (v.default == 0.0) {
+      return (sum(v.activeValues),domainsize);
+    } else if (v.domain.isFinite) {
+      // non-zero default but finite domain
+      val (sum,count) = sumcount(v.activeValues);
+      return (sum + ((domainsize - count) * v.default), domainsize);
+    } else {
+      // infinite domain with non-zero default
+      return (Math.signum(v.default) * Double.PositiveInfinity, domainsize);
+    }
   }
-
+  
+  /** Returns the sum of the elements of iterator. */
+  def sum(v : Iterator[Double]) : Double =
+    sumcount(v)._1;
+  
+  /** Returns the sum of the elements of collection. */
+  def sum(v : Collection[Double]) : Double =
+    sum(v.elements);
+  
+  /** Returns the sum of the values of the map. */
+  def sum[I](v : PartialMap[I,Double]) : Double =
+    sumcount(v)._1;
+  
+  /** Returns the sum of the values of the value of the operation. */
+  def sum[I,T<:Tensor[I]](op : TensorOp[I,T]) : Double =
+    sum(op.value);
+  
+  /** Log each element of a vector or matrix */
+  def log[I](v : PartialMap[I,Double]) = v.map(Math.log _);
+  def log(v : Iterator[Double]) = v.map(Math.log);
+  def log[S<:Collection[Double]](v : S) = v.map(Math.log);
+  def log[I,T<:Tensor[I]](op : TensorOp[I,T]) : Double =
+    log(op.value);
+  
   /** The maximum active value of the map (does not consider default). */
   def max[I](v : PartialMap[I,Double]) : Double =
-    v.activeValues.foldLeft(Double.MinValue)(Math.max);
+    v.activeValues.reduceLeft(Math.max);
+  def max(v : Iterator[Double]) : Double = v.reduceLeft(Math.max);
+  def max(v : Collection[Double]) : Double = max(v.elements);
+  def max[I,T<:Tensor[I]](op : TensorOp[I,T]) : Double = max(op.value);
   
   /** The minimum active value of the map (does not consider default). */
   def min[I](v : PartialMap[I,Double]) : Double =
-    v.activeValues.foldLeft(Double.MaxValue)(Math.min);
+    v.activeValues.reduceLeft(Math.min);
 
+  def min(v : Iterator[Double]) : Double = v.reduceLeft(Math.min);
+  def min(v : Collection[Double]) : Double = min(v.elements);
+  def min[I,T<:Tensor[I]](op : TensorOp[I,T]) : Double = min(op.value);
+  
   /**
    * Returns the sum of the squares of the elements of the vector.
    */
-  def sumsq[I](v : PartialMap[I,Double]) : Double =
-    sum(v.map(x => x*x));
+  def sumsq[I](v : PartialMap[I,Double]) : Double = sum(v.map((x:Double) => x*x));
+  def sumsq(v : Iterator[Double]) = sum(v.map(x => x*x));
+  def sumsq(v : Seq[Double]) = sum(v.map(x => x*x));
+  def sumsq[I,T<:Tensor[I]](op : TensorOp[I,T]) : Double = sumsq(op.value);
   
   /** Returns the mean of the vector: sum(v) / v.size. */
-  def mean[I](v : PartialMap[I,Double]) : Double =
-    sum(v) / v.domain.asInstanceOf[scalala.collection.domain.FiniteDomain[_]].size;
+  def mean[I](v : PartialMap[I,Double]) : Double = {
+    val (sum,count) = sumcount(v);
+    if (count < 0)
+      throw new IllegalArgumentException("Cannot take mean of infinite domain");
+    sum / count;
+  }
+  
+  /** Returns the mean of the given elements. */
+  def mean(v : Iterator[Double]) : Double = {
+    val (sum,count) = sumcount(v);
+    sum / count;
+  }
+  
+  /** Returns the mean of the given elements. */
+  def mean(v : Seq[Double]) : Double = mean(v.elements);
+  
+  def mean[I,T<:Tensor[I]](op : TensorOp[I,T]) : Double = mean(op.value);
   
   /** Returns the sum vector of a bunch of vectors. */
   def sum(vectors : Seq[Vector]) : Vector = {
@@ -109,20 +168,20 @@ trait Vectors extends Library with Operators {
    * sqrt(sumsq (v - mean(v)) / (v.size - 1)).
    */
   def std(v : Vector) : Double =
-    sqrt(sumsq(v - mean(v)) / (v.size - 1));
+    sqrt(sumsq(v - mean(v) value) / (v.size - 1));
   
   /** Returns the n'th euclidean norm of the given vector. */
   def norm[I](v : PartialMap[I,Double], n : Double) : Double = {
     if (n == 1) {
-      return sum(v.map(Math.abs));
+      return sum(v.map((x:Double) => Math.abs(x)));
     } else if (n == 2) {
-      return sqrt(sum(v.map(x => x * x)));
+      return sqrt(sum(v.map((x:Double) => x * x)));
     } else if (n % 2 == 0) {
-      return Math.pow(sum(v.map(x => Math.pow(x, n))), 1.0 / n);
+      return Math.pow(sum(v.map((x:Double) => Math.pow(x, n))), 1.0 / n);
     } else if (n % 2 == 1) {
-      return Math.pow(sum(v.map(x => Math.pow(Math.abs(x), n))), 1.0 / n);
+      return Math.pow(sum(v.map((x:Double) => Math.pow(Math.abs(x), n))), 1.0 / n);
     } else if (n == Double.PositiveInfinity) {
-      return max(v.map(Math.abs));
+      return max(v.map((x:Double) => Math.abs(x)));
     } else {
       throw new UnsupportedOperationException();
     }
