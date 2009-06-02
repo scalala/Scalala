776dfc8d081f058d3132b546f1a516ee535285b8
Daniel Ramage <dramage@cs.stanford.edu>
2009-06-02 13:55:36 -0700

    rearchitected transpose

--- src/scalala/tensor/Matrix.scala
+++ src/scalala/tensor/Matrix.scala
@@ -47,6 +47,10 @@ trait Matrix extends Tensor2[Int,Int] {
   private val _domain = ProductSet(IntSpanSet(0, rows), IntSpanSet(0, cols));
   final override def domain = _domain;
   
+  override def transpose : Matrix = new {
+    val inner : Matrix = Matrix.this;
+  } with Matrix.Transpose;
+  
   override def getRow(row : Int) = new Vector {
     override def size = cols;
     override def apply(i : Int) = Matrix.this.apply(row,i);
@@ -76,4 +80,50 @@ trait Matrix extends Tensor2[Int,Int] {
   }
   */
   
+}
+
+object Matrix {
+  trait Transpose extends Matrix {
+    type I1 = Int;
+    type I2 = Int;
+    
+    val inner : Matrix;
+    
+    override def rows = inner.cols;
+    override def cols = inner.rows;
+    
+    override def apply(row : I2, col : I1) =
+      inner.apply(col, row);
+      
+    override def update(row : I2, col : I1, value : Double) =
+      inner.update(col, row, value);
+    
+    override def copy =
+      inner.copy.asInstanceOf[Matrix].transpose;
+    
+    override def create[J](domain : MergeableSet[J]) =
+      inner.create(domain);
+      
+    override def activeDomain : MergeableSet[(I2,I1)] = {
+      new MergeableSet[(I2,I1)] {
+        override def size = inner.activeDomain.size;
+        override def elements = inner.activeDomain.elements.map(tup => (tup._2,tup._1));
+        override def contains(i : (I2,I1)) = inner.activeDomain.contains((i._2,i._1));
+      };
+    }
+    
+    override def transpose = inner;
+    
+    override def getRow(col : I2) =
+      inner.getCol(col);
+    
+    override def getCol(row : I1) =
+      inner.getRow(row);
+    
+    override def activeDomainInRow(col : I2) =
+      inner.activeDomainInCol(col);
+    
+    override def activeDomainInCol(row : I1) =
+      inner.activeDomainInRow(row);
+  }
 }
