2d6c109a8c549c6bea3ec1cd29d6712de252e467
Daniel Ramage <dramage@cs.stanford.edu>
2010-05-08 22:23:04 -0700

    un-borked the scalala.sh startup script

--- scalala.sh
+++ scalala.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 BASE=`readlink -f $0 | xargs dirname`;
-JAR=`find target/ -name 'scalala-*-jar-with-dependencies.jar' | head -1`
+JAR=`find $BASE/target/ -name 'scalala*-jar-with-dependencies.jar' 2>/dev/null | head -1`
 
 # find jar in target/
-if [ ! -e $JAR ] ; then
-  echo "Could not find target/scalala-*-jar-with-dependencies.jar."
+if [ ! -e "$JAR" ] ; then
+  echo "Could not find target/scalala*-jar-with-dependencies.jar."
   echo "Make sure to run mvn assembly:assembly first."
   exit 1;
 fi
 
-# find SCALA_HOME if none set and scala is in the path
-if [ -z "$SCALA_HOME" ] ; then
-  SCALA_HOME=`which scala | xargs dirname | xargs dirname`
-fi
+## find SCALA_HOME if none set and scala is in the path
+#if [ -z "$SCALA_HOME" ] ; then
+#  SCALA_HOME=`which scala | xargs dirname | xargs dirname`
+#fi
 
 # set up classpath, including jline if available
-export CLASSPATH=$CLASSPATH:$JAR
-if [ -e "$SCALA_HOME/lib/jline.jar" ] ; then
-  export CLASSPATH=$CLASSPATH:$SCALA_HOME/lib/jline.jar
-fi
+export CLASSPATH=$JAR
+#if [ -e "$SCALA_HOME/lib/jline.jar" ] ; then
+#  export CP=$CP:$SCALA_HOME/lib/jline.jar
+#fi
 
-java -cp $JAR:$CLASSPATH scalala.ScalalaConsole
+scala scalala.ScalalaConsole
 
