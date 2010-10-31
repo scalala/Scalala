b71b6cc9a9f342ba427a73d726934662267c2492
Daniel Ramage <dramage@cs.stanford.edu>
2010-10-31 13:10:54 -0700

    cleaned dead code branches and re-added scalar aliases that were carelessly removed

--- src/main/scala/scalala/operators/Ops.scala
+++ src/main/scala/scalala/operators/Ops.scala
@@ -25,7 +25,6 @@ import generic._;
 import generic.collection.CanTranspose;
 
 import scalala.tensor.Scalar;
-import scalala.tensor.domain.DomainException;
 import scalala.collection.sparse.{SparseArray,DefaultArrayValue};
 
 /**
@@ -74,6 +73,18 @@ trait NumericOps[+This] {
   /** Alias for :-=(b) */
   final def -[TT>:This,B,That](b : B)(implicit op : CanSub[TT,B,That], sb : Scalar[B]) =
     this.:-[TT,B,That](b);
+
+  /** Alias for :*=(b) */
+  final def *[TT>:This,B,That](b : B)(implicit op : CanMul[TT,B,That], sb : Scalar[B]) =
+    this.:*[TT,B,That](b);
+
+  /** Alias for :/=(b) */
+  final def /[TT>:This,B,That](b : B)(implicit op : CanDiv[TT,B,That], sb : Scalar[B]) =
+    this.:/[TT,B,That](b);
+
+  /** Alias for :%=(b) */
+  final def %[TT>:This,B,That](b : B)(implicit op : CanMod[TT,B,That], sb : Scalar[B]) =
+    this.:%[TT,B,That](b);
 }
 
 /**
@@ -150,167 +161,6 @@ trait RowOps[+This] extends NumericOps[This] {
 }
 
 /**
- * Secialized NumericOps with shaped operations taking A is a column.
- * Note that columns are the default shape, so this trait simply extends
- * NumericOps[A].
- *
- * @author dramage
- */
-trait ColumnTensorOps[+This] extends NumericOps[This] {
-  def *[TT>:This,B,RV](b : RowTensorOps[B])(implicit op : CanMulColumnBy[TT,B,RV]) : RV =
-    op(repr,b.column);
-
-  def t : RowTensorOps[This] = RowTensorOps(repr);
-}
-
-/**
- * A column tensor whose underyling collection is mutable.  This trait
- * should be used instead of mixing in "ColumnTensorOps with
- * MutableNumeriCollectionOps" directly, because .t needs to return an instance
- * of MutableRowTensorOps insetad of RowTensorOps.
- *
- * @author dramage
- */
-trait MutableColumnTensorOps[+This] extends ColumnTensorOps[This] with MutableNumericOps[This] {
-  override def t : MutableRowTensorOps[This] = MutableRowTensorOps(repr);
-}
-
-
-/*
- * Secialized NumericOps with shaped operations taking A is a row.
- * Note that there is an inherent asymmetry between ColumnTensorOps and
- * RowTensorOps: because tensors are assumed to be columns until reshaped
- * (e.g. by calling .t), that class extends NumericOps[A].  This
- * class, by contrast, must preserve the fact that the base numeric operations
- * like plus must honor the row shape, and that the return result should also
- * be a row.  Hence this class extends NumericOps[RowTensorOps[A]]
- * and provides implicit magic in the companion object to wrap the
- * corresponding construction delegates.
- *
- * @author dramage
- */
-trait RowTensorOps[+This] extends NumericOps[RowTensorOps[This]] {
-  override def repr : RowTensorOps[This] = this;
-
-  def column : This;
-
-  def *[TT>:This,B,RV](b : B)(implicit op : CanMulRowBy[TT,B,RV]) : RV =
-    op(this.column,b);
-
-  /** The transpose returns the underlying value, which assumed to be a column. */
-  def t : This = column;
-}
-
-object RowTensorOps {
-  def apply[This](v : This) : RowTensorOps[This] =
-    new RowTensorOps[This] { override def column = v; }
-
-  class RowBinaryOp[A,-B,+That](implicit op : BinaryOp[A,B,That])
-  extends BinaryOp[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]] {
-    override def apply(a : RowTensorOps[A], b : RowTensorOps[B]) =
-      RowTensorOps(op(a.column,b.column));
-  }
-
-  implicit def CanAddRows[A,B,That](implicit op : CanAdd[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanAdd[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanSubRows[A,B,That](implicit op : CanSub[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanSub[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanMulRows[A,B,That](implicit op : CanMul[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanMul[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanDivRows[A,B,That](implicit op : CanDiv[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanDiv[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanModRows[A,B,That](implicit op : CanMod[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanMod[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanPowRows[A,B,That](implicit op : CanPow[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanPow[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanLTRows[A,B,That](implicit op : CanLT[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanLT[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanLTERows[A,B,That](implicit op : CanLTE[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanLTE[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanGTRows[A,B,That](implicit op : CanGT[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanGT[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanGTERows[A,B,That](implicit op : CanGTE[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanGTE[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanEqRows[A,B,That](implicit op : CanEq[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanEq[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-
-  implicit def CanNeRows[A,B,That](implicit op : CanNe[A,B,That])
-  = new RowBinaryOp[A,B,That] with CanNe[RowTensorOps[A],RowTensorOps[B],RowTensorOps[That]];
-}
-
-/**
- * Specialized RowTensorOps support for RowTensors that have mutable
- * underlying collections.
- *
- * @author dramage
- */
-trait MutableRowTensorOps[+This]
-extends RowTensorOps[This] with MutableNumericOps[RowTensorOps[This]];
-
-object MutableRowTensorOps {
-  def apply[This](v : This) : MutableRowTensorOps[This] =
-    new MutableRowTensorOps[This] { override def column = v; }
-
-  class MutableRowBinaryOp[A,-B,+That](implicit op : BinaryOp[A,B,That])
-  extends BinaryOp[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]] {
-    override def apply(a : MutableRowTensorOps[A], b : RowTensorOps[B]) =
-      MutableRowTensorOps(op(a.column,b.column));
-  }
-
-  class MutableRowBinaryUpdateOp[A,-B](implicit op : BinaryUpdateOp[A,B])
-  extends BinaryUpdateOp[MutableRowTensorOps[A],RowTensorOps[B]] {
-    override def apply(a : MutableRowTensorOps[A], b : RowTensorOps[B]) =
-      op(a.column, b.column);
-  }
-
-  implicit def CanAddRows[A,B,That](implicit op : CanAdd[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanAdd[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanSubRows[A,B,That](implicit op : CanSub[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanSub[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanMulRows[A,B,That](implicit op : CanMul[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanMul[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanDivRows[A,B,That](implicit op : CanDiv[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanDiv[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanModRows[A,B,That](implicit op : CanMod[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanMod[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanPowRows[A,B,That](implicit op : CanPow[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanPow[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanLTRows[A,B,That](implicit op : CanLT[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanLT[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanLTERows[A,B,That](implicit op : CanLTE[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanLTE[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanGTRows[A,B,That](implicit op : CanGT[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanGT[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanGTERows[A,B,That](implicit op : CanGTE[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanGTE[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanEqRows[A,B,That](implicit op : CanEq[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanEq[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-
-  implicit def CanNeRows[A,B,That](implicit op : CanNe[A,B,That])
-  = new MutableRowBinaryOp[A,B,That] with CanNe[MutableRowTensorOps[A],RowTensorOps[B],MutableRowTensorOps[That]];
-}
-
-/**
  * Provides matrix-like operations for two dimensional collections.
  *
  * @author dramage
@@ -362,14 +212,29 @@ class RichScalar[@specialized(Int,Long,Float,Double) A:Scalar]
 (override val repr : A)
 extends NumericOps[A];
 
+/**
+ * Numeric operator support for tuples of numeric values.
+ *
+ * @author dramage
+ */
 class RichTuple2[@specialized A, @specialized B]
 (override val repr : (A,B))
 extends NumericOps[(A,B)];
 
+/**
+ * Numeric operator support for tuples of numeric values.
+ *
+ * @author dramage
+ */
 class RichTuple3[@specialized A, @specialized B, @specialized C]
 (override val repr : (A,B,C))
 extends NumericOps[(A,B,C)];
 
+/**
+ * Numeric operator support for tuples of numeric values.
+ *
+ * @author dramage
+ */
 class RichTuple4[@specialized A, @specialized B, @specialized C, @specialized D]
 (override val repr : (A,B,C,D))
 extends NumericOps[(A,B,C,D)];
@@ -382,6 +247,13 @@ extends NumericOps[(A,B,C,D)];
 class RichMap[K,V](override val repr : Map[K,V])
 extends NumericOps[Map[K,V]];
 
+/**
+ * Implicit promotions of built-in scala types to enriched numerically valued
+ * equivalents.  After importing the members of this class, you can directly
+ * do math on all tuples, arrays, and maps.
+ *
+ * @author dramage
+ */
 object Implicits {
   implicit def richScalar[@specialized(Int,Long,Float,Double) V:Scalar](value : V) =
     new RichScalar(value);
