1fcf06cfbdcc521901404eb08e69ad533a509a99
David Hall <dlwh@berkeley.edu>
2011-04-15 17:29:13 -0700

    DiagonalMatrix, along with a function diag in LinearAlgebra

---
+++ src/main/scala/scalala/tensor/DiagonalMatrix.scala
@@ -0,0 +1,20 @@
+package scalala
+package tensor
+
+import domain.TableDomain
+import scalar.Scalar
+
+/**
+ * 
+ * @author dlwh
+ */
+class DiagonalMatrix[Vec,S](val innerVector: Vec)(implicit val scalar: Scalar[S], view: Vec<:<Vector[S])
+  extends Matrix[S] with MatrixLike[S,DiagonalMatrix[Vec,S]] {
+  def domain: TableDomain = new TableDomain(innerVector.size,innerVector.size);
+  def apply(i: Int, j: Int): S = {
+    innerVector.checkKey(i);
+    innerVector.checkKey(j);
+    if(i == j) innerVector(i) else scalar.zero
+  }
+
+}
