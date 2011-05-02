3549ad27a925c88f965eec553e0738fd4be314e5
Daniel Ramage <dramage@cs.stanford.edu>
2011-05-01 20:55:08 -0700

    scalatest ought to use sbt cross builds ...

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -22,8 +22,15 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
   val JFreeChart = "jfree" % "jfreechart" % "1.0.13"
   val XMLGraphicsCommons = "org.apache.xmlgraphics" % "xmlgraphics-commons" % "1.3.1"
   val IText = "com.lowagie" % "itext" % "2.1.5" intransitive()
-  val ScalaCheck = "org.scala-tools.testing" % "scalacheck_2.9.0.RC1" % "1.8" % "test"
-  val ScalaTest = "org.scalatest" % "scalatest" % "1.4.RC2" % "test"
+
+  val ScalaCheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
+
+  val ScalaTest = buildScalaVersion match {
+    case "2.9.0.RC2" => "org.scalatest" % "scalatest" % "1.4.RC2" % "test"
+    case "2.8.1"     => "org.scalatest" % "scalatest" % "1.3" % "test"
+    case x           => error("Unsupported Scala version " + x)
+  }
+
   val JUnit = "junit" % "junit" % "4.5" % "test"
 
   //
