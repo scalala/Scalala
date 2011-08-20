9c553c103ee5895b87efef5531c29ee454a1a78b
David Hall <dlwh@berkeley.edu>
2011-08-19 20:31:32 -0700

    Revert "added linspace to DenseVector"
    I don't know why this breaks the build.
    
    This reverts commit 67b0ace498a51ebd265c1fa3c85d7d7ae8b567af.

--- src/main/scala/scalala/tensor/dense/DenseVector.scala
+++ src/main/scala/scalala/tensor/dense/DenseVector.scala
@@ -568,12 +568,5 @@ trait DenseVectorColConstructors {
     tabulate(size)(i => mt.nextInt(imax));
   }
 
-   /***
-    * n evenly spaced points between a and b
-    */
-  def linspace(a: Double, b: Double, n: Int= 100):DenseVectorCol[Double] = {
-    tabulate(n)(i => a + (b - a) / n * i)
-  }
-
 }
 
