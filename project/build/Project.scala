1610a3be0b32a042d5e2eb4b98d28ba67c395599
Daniel Ramage <dramage@cs.stanford.edu>
2011-09-14 13:38:27 -0700

    up to scala 2.9.1

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -22,12 +22,16 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
   val XMLGraphicsCommons = "org.apache.xmlgraphics" % "xmlgraphics-commons" % "1.3.1"
   val IText = "com.lowagie" % "itext" % "2.1.5" intransitive()
 
-  val ScalaCheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
+  val ScalaCheck = buildScalaVersion match {
+    case "2.9.1" => "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
+    case "2.8.1" => "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
+    case x       => error("Unsupported Scala version " + x)
+  }
 
   val ScalaTest = buildScalaVersion match {
-    case "2.9.0"     => "org.scalatest" % "scalatest" % "1.4.RC2" % "test"
-    case "2.8.1"     => "org.scalatest" % "scalatest" % "1.3" % "test"
-    case x           => error("Unsupported Scala version " + x)
+    case "2.9.1" => "org.scalatest" % "scalatest" % "1.4.RC2" % "test"
+    case "2.8.1" => "org.scalatest" % "scalatest" % "1.3" % "test"
+    case x       => error("Unsupported Scala version " + x)
   }
 
   val JUnit = "junit" % "junit" % "4.5" % "test"
